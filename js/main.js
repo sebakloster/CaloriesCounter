2da8f95a3f0bff4a80078a68785f52556af58234
Seba Kloster <sebikloster@yahoo.com.ar>
2021-01-13 10:46:40 -0300

    Agregando pie chart

--- js/main.js
+++ js/main.js
@@ -135,3 +135,29 @@ const removeItem = (index) => {
   updateTotals();
   renderItems();
 };
+
+$(document).ready(function () {
+  var ctx = $("#chart-line");
+  var myLineChart = new Chart(ctx, {
+    type: "pie",
+    data: {
+      labels: ["Calories", "Carbs", "Protein"],
+      datasets: [
+        {
+          data: [1200, 1700, 800],
+          backgroundColor: [
+            "rgba(255, 0, 0, 0.5)",
+            "rgba(100, 255, 0, 0.5)",
+            "rgba(200, 50, 255, 0.5)",
+          ],
+        },
+      ],
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Weather",
+      },
+    },
+  });
+});
