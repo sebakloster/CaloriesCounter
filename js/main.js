0f5eb3b4322ffb2fcec30a00610887292dca0f25
Seba Kloster <sebikloster@yahoo.com.ar>
2021-01-13 10:39:12 -0300

    Crenado fuciones para crear y eliminar las filas, creando funcion para actualizar los totales, agregando icono de eliminar fila

--- js/main.js
+++ js/main.js
@@ -6,6 +6,39 @@ const compose = (...functions) => (data) =>
 // let carbs = $("#carbs");
 // let protein = $("#protein");
 
+const attributesToString = (obj = {}) => {
+  const keys = Object.keys(obj);
+  const attrs = [];
+
+  for (let i = 0; i < keys.length; i++) {
+    let attr = keys[i];
+    attrs.push(`${attr}="${obj[attr]}"`);
+  }
+  const string = attrs.join("");
+  return string;
+};
+
+const tagAttrs = (obj) => (content = "") =>
+  `<${obj.tag}${obj.attrs ? " " : ""}${attributesToString(
+    obj.attrs
+  )}>${content}</>`;
+
+const tag = (t) => {
+  if (typeof t === "string") {
+    return tagAttrs({ tag: t });
+  } else {
+    return tagAttrs(t);
+  }
+};
+
+const tableRowTag = tag("tr");
+const tableRow = (items) => compose(tableRowTag, tableCells)(items);
+
+const tableCell = tag("td");
+const tableCells = (item) => item.map(tableCell).join("");
+
+const trashIcon = tag({ tag: "i", attrs: { class: "fas fa-trash-alt" } })("");
+
 let description = document.getElementById("description");
 let calories = document.getElementById("calories");
 let carbs = document.getElementById("carbs");
@@ -40,13 +73,29 @@ const validateInputs = () => {
 const add = () => {
   const newItem = {
     description: description.value,
-    calories: calories.value,
-    carbs: carbs.value,
-    protein: protein.value,
+    calories: Number(calories.value),
+    carbs: Number(carbs.value),
+    protein: Number(protein.value),
   };
   list.push(newItem);
   cleanInputs();
-  console.log(list);
+  updateTotals();
+  renderItems();
+};
+
+const updateTotals = () => {
+  let calories = 0;
+  let carbs = 0;
+  let protein = 0;
+
+  list.map((item) => {
+    (calories += item.calories),
+      (carbs += item.carbs),
+      (protein += item.protein);
+  });
+  document.getElementById("totalCalories").textContent = calories;
+  document.getElementById("totalCarbs").textContent = carbs;
+  document.getElementById("totalProtein").textContent = protein;
 };
 
 const cleanInputs = () => {
@@ -54,4 +103,35 @@ const cleanInputs = () => {
   carbs.value = "";
   calories.value = "";
   protein.value = "";
+};
+const renderItems = () => {
+  document.querySelector("tbody").innerHTML = "";
+
+  list.map((item, index) => {
+    const removeButton = tag({
+      tag: "button",
+      attrs: {
+        class: "btn btn-outline-danger",
+        onclick: `removeItem(${index})`,
+      },
+    })(trashIcon);
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = tableRow([
+      item.description,
+      item.calories,
+      item.carbs,
+      item.protein,
+      removeButton,
+    ]);
+
+    document.querySelector("tbody").appendChild(row);
+  });
+};
+
+const removeItem = (index) => {
+  list.splice(index, 1);
+  updateTotals();
+  renderItems();
 };
